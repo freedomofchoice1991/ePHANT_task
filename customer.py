c5bf61522fe20cc1c67dc16fed9f5126a3259eb6
Reza Karami <reza.karami.arbeiten@gmail.com>
2022-12-06 10:38:54 +0100

    Create customer.py
    Code related to Customer class and generation of 2000 random instances of that class

---
+++ customer.py
@@ -0,0 +1,44 @@
+ 
+from cmath import sin
+import random
+from generateRandomCustomerNames import generateName  
+
+
+class Customer:
+
+    def __init__(self, name, money, type):
+        self.name = name
+        self.money = money
+        self.type = type
+    
+    def comeInsideRestaurant (self,restaurant_number_of_seats):
+        # print('Entered Rest')
+        restaurant_number_of_seats -= 1
+        return restaurant_number_of_seats
+         
+
+    def exitFromRestaurant (self, restaurant_number_of_seats):
+        # print('Exited')
+        restaurant_number_of_seats += 1
+        return restaurant_number_of_seats
+
+    def payTheBill (self,):
+        # print('Bill is paied')
+        self.money -= 10
+        return self.money
+
+
+
+##----Generate 2k random customers----
+
+pairCustomerPool = []
+singleCustomerPool = []
+for i in range(1,2001):
+    x = 'c'+'i'
+    x = Customer(generateName(),random.randint(50,100), type(generateName()))
+    if x.type is tuple:
+        pairCustomerPool.append(x)
+    else:
+        singleCustomerPool.append(x)    
+
+##--------
